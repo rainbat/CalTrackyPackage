eda9f414454cb4fe04b0520338aebf47520574bc
Dominique Dümmler <info@rainbat.ch>
2023-03-19 12:59:22 +0100

    dep

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "CalTrackyPackage",
-            dependencies: []),
+            dependencies: ["SwiftyJSON"]),
         .testTarget(
             name: "CalTrackyPackageTests",
             dependencies: ["CalTrackyPackage"]),
